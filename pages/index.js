96eff4f6856fdcce3c65077009ccf271ab9943e3
Talha Saleem <talha.sameed@gmail.com>
2024-12-15 09:14:27 +0500

    Moved handleshare to utils

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { FontPicker } from "@/ui/embla";
-import { detectIOS, FONTS, generateImage } from "@/ui/utils";
+import { detectIOS, FONTS, handleShare } from "@/ui/utils";
 import Head from "next/head";
 import { useState, useEffect } from "react";
 
@@ -18,95 +18,6 @@ export default function Home() {
         setIsIOS(detectIOS());
 
     }, []);
-
-    // Function to handle sharing
-    const handleShare = async () => {
-
-        // Generate the image data URL
-        const dataUrl = generateImage(value, font);
-
-        // Check if the device is iOS
-        if (isIOS) {
-
-            try {
-
-                // Generate the file object
-                const blob = await (await fetch(dataUrl)).blob();
-                const file = new File([blob], 'text-image.png', { type: 'image/png' });
-                
-                // Check if the Web Share API is supported
-                if (navigator.share) {
-
-                    // Share the file
-                    await navigator.share({
-                        files: [file]
-                    });
-
-                    // Alert the user
-                    setCopied(true);
-
-                    // Reset after 2 seconds
-                    setTimeout(() => setCopied(false), 2000);
-                } else {
-
-                    // Throw an error
-                    throw new Error('Web Share API not supported');
-                }
-            } catch (error) {
-
-                // Handle errors
-                console.error("Error sharing:", error);
-                
-                // Alert the user
-                alert("Sharing failed. Please try copying the image manually.");
-            }
-        } else {
-            
-            // For Android and other devices, 
-            // use the existing clipboard functionality
-            try {
-
-                // Generate the response from the image data URL
-                const response = await fetch(dataUrl);
-                const blob = await response.blob();
-
-                // Check if the clipboard API is supported
-                if (navigator.clipboard && navigator.clipboard.write) {
-                    
-                    // Write the blob to the clipboard
-                    await navigator.clipboard.write([
-                        new ClipboardItem({
-                            [blob.type]: blob
-                        })
-                    ]);
-
-                    // Alert the user
-                    setCopied(true);
-
-                    // Reset after 2 seconds
-                    setTimeout(() => setCopied(false), 2000);
-
-                } else {
-
-                    // Throw an error
-                    throw new Error('Clipboard API not supported');
-                }
-            } catch (error) {
-
-                // Handle errors
-                console.error("Failed to copy image to clipboard", error);
-                alert("Failed to copy image. Please try saving it manually.");
-
-                // Fallback to download
-                const link = document.createElement('a');
-                link.href = dataUrl;
-                link.download = `${value}.png`;
-                link.click();
-            }
-        }
-    };
-
-    // Fonts
    
       
     // Markup
@@ -133,13 +44,15 @@ export default function Home() {
 
         {/* Footer */}
         <div className="fixed bottom-0 left-0 right-0 z-10 flex items-center justify-center h-16 py-12 px-10 text-white">
+
             {/* Download button */}
             <button
                 className="btn w-full px-4 py-3 rounded-2xl"
-                onClick={() => handleShare()}
+                onClick={() => handleShare(value, font, isIOS, setCopied)}
             >
                 {copied ? "Copied!" : "Copy to Clipboard"}
             </button>
+
         </div>
 
     </div>
--- ui/utils.js
+++ ui/utils.js
@@ -36,3 +36,91 @@ export const FONTS = [
     { name: "Sacramento", value: "--font-sacramento" },
     { name: "Zeyada", value: "--font-zeyada" }
 ];
+
+// Function to handle sharing
+export const handleShare = async (value, font, isIOS, setCopied) => {
+
+    // Generate the image data URL
+    const dataUrl = generateImage(value, font);
+
+    // Check if the device is iOS
+    if (isIOS) {
+
+        try {
+
+            // Generate the file object
+            const blob = await (await fetch(dataUrl)).blob();
+            const file = new File([blob], 'text-image.png', { type: 'image/png' });
+            
+            // Check if the Web Share API is supported
+            if (navigator.share) {
+
+                // Share the file
+                await navigator.share({
+                    files: [file]
+                });
+
+                // Alert the user
+                setCopied(true);
+
+                // Reset after 2 seconds
+                setTimeout(() => setCopied(false), 2000);
+            } else {
+
+                // Throw an error
+                throw new Error('Web Share API not supported');
+            }
+        } catch (error) {
+
+            // Handle errors
+            console.error("Error sharing:", error);
+            
+            // Alert the user
+            alert("Sharing failed. Please try copying the image manually.");
+        }
+    } else {
+        
+        // For Android and other devices, 
+        // use the existing clipboard functionality
+        try {
+
+            // Generate the response from the image data URL
+            const response = await fetch(dataUrl);
+            const blob = await response.blob();
+
+            // Check if the clipboard API is supported
+            if (navigator.clipboard && navigator.clipboard.write) {
+                
+                // Write the blob to the clipboard
+                await navigator.clipboard.write([
+                    new ClipboardItem({
+                        [blob.type]: blob
+                    })
+                ]);
+
+                // Alert the user
+                setCopied(true);
+
+                // Reset after 2 seconds
+                setTimeout(() => setCopied(false), 2000);
+
+            } else {
+
+                // Throw an error
+                throw new Error('Clipboard API not supported');
+            }
+        } catch (error) {
+
+            // Handle errors
+            console.error("Failed to copy image to clipboard", error);
+            alert("Failed to copy image. Please try saving it manually.");
+
+            // Fallback to download
+            const link = document.createElement('a');
+            link.href = dataUrl;
+            link.download = `${value}.png`;
+            link.click();
+        }
+    }
+};
+
